a833a5f72e0a9fc3bdc4005ac82fec661f385b78
Gukasio <m2405094@edu.misis.ru>
2025-05-29 13:48:39 +0300

    Add files via upload

--- Model/Data/SerializerJSON.cs
+++ Model/Data/SerializerJSON.cs
@@ -10,11 +10,19 @@ public class SerializerJSON : Serializer
     public override void Save(World world)
     {
         var json = JObject.FromObject(new WorldDTO(world));
-        File.WriteAllText(FilePath,json.ToString());
+        File.WriteAllText(FilePath, json.ToString());
     }
     public override void Load(World world)
     {
         var content = File.ReadAllText(FilePath);
         var deser = JObject.Parse(content);
+        var PlayerX = deser["PosistionPlayerX"].ToObject<int>();
+        var PlayerY = deser["PositionPlayerY"].ToObject<int>();
+        var PlatformX = deser["PositionPlatformX"].ToObject<int[]>();
+        var PlatformY = deser["PositionPlatformY"].ToObject<int[]>();
+        var Score = deser["Score"].ToObject<int>();
+        world.LoadWorld(PlayerX, PlayerY, PlatformX,PlatformY,Score);
+        
     }
+    
 }
--- Model/Data/WorldDTO.cs
+++ Model/Data/WorldDTO.cs
@@ -5,7 +5,7 @@ namespace Model.Data;
 
 public class WorldDTO
 {
-    public int PosistionPLayerX { get; set; }
+    public int PosistionPlayerX { get; set; }
     public int PosistionPlayerY { get; set; }
     public int[] PositionPlatformX { get; set; }
     public int[] PositionPlatformY { get; set; }
@@ -13,14 +13,16 @@ public class WorldDTO
     public WorldDTO() { }
     public WorldDTO(World world)
     {
-        PosistionPLayerX = (int)world.Player.X;
+        PosistionPlayerX = (int)world.Player.X;
         PosistionPlayerY = (int)world.Player.Y;
         int dlinamassiva = world.Platforms.Length;
+        PositionPlatformX = new int[dlinamassiva];
+        PositionPlatformY = new int[dlinamassiva];
         for (int i = 0; i < dlinamassiva; i++)
         {
             PositionPlatformX[i] = (int)world.Platforms[i].X;
             PositionPlatformY[i] = (int)world.Platforms[i].Y;
         }
-        Score = 0;
+        Score = world.Score;
     }
 }
